cfe8545812ffd0baa5638ef343d0e2563cd3711a
vadwwxz <vadwwxz@gmail.com>
2023-03-11 16:43:10 +0200

    Created new module - generation_from_text

--- generator/src/main/java/com/main/generate/presentation/ui/GeneratorFragment.kt
+++ generator/src/main/java/com/main/generate/presentation/ui/GeneratorFragment.kt
@@ -1,12 +1,10 @@
 package com.main.generate.presentation.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.main.core.base.BaseFragment
-import com.main.generate.R
 import com.main.generate.databinding.FragmentGeneratorBinding
 
 class GeneratorFragment : BaseFragment() {
